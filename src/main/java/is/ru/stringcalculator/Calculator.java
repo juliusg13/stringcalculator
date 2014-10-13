43312ad6ba54a95b1ea3af1e55aabf515b2f3f71
juliusg13 <juliusg13@ru.is>
2014-10-13 16:57:57 +0000

    added function for new lines.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -18,9 +18,10 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	    return numbers.split("(,)|(\n)");
 	}
-      
+
+ 
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
