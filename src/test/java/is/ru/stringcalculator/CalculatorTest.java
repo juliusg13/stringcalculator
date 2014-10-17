62f49de040a2124b2c0b75b69eff6c742e8bb2e9
juliusg13 <juliusg13@ru.is>
2014-10-17 18:35:50 +0000

    added test case for multiple any length denominating terminiators

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -73,6 +73,10 @@ public class CalculatorTest {
 	public void testForMultipeDelim() {
 		assertEquals(6, Calculator.add("//[*][%]\n1*2%3"));
 	}
+	@Test
+	public void testForMultipleAnySizeDelim() {
+		assertEquals(6, Calculator.add("//[**][%%%%]\n1**2%%%%3"));
+	}
 
 
 }
