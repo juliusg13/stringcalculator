c59701c611fee94a52a24d8c92539d294cd8491d
juliusg13 <juliusg13@ru.is>
2014-10-17 15:55:18 +0000

    implemented for multiple deliminators

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -24,14 +24,33 @@ public class Calculator {
 	    return numbers.split("(,)|(\n)");
 	}
 	private static String[] splitDelims(String text){
-		String newText = text.replaceAll("\n", "");
-		String delim = text.substring(2, 3);
-		String newer = newText.replaceAll(delim, ",");
-		String Newest = newer.replaceAll("/", "");
-		String returner = Newest.replaceFirst(",", "");
+		String delim = "";
+		String text1 = "";
 
-		return (splitNumbers(returner));
-	}
+		if(text.contains("[")){
+
+			int dindex = text.indexOf("]");
+			String Tdelim = text.substring(3, dindex);
+			for(int i = 0; i < Tdelim.length(); i++)
+			{
+				delim = delim + "\\" + Tdelim.substring(i, i+1);
+			}
+			text1 = text.substring(dindex+2, text.length());
+			String newText = text1.replaceFirst("\n", "");
+			String returner = newText.replaceAll(delim, ",");
+			return(splitNumbers(returner));
+		}
+		else {
+			delim = text.substring(2, 3);
+			text1 = text;
+			String newText = text1.replaceFirst("\n", "");
+			String newer = newText.replaceAll(delim, ",");
+			String Newest = newer.replaceAll("/", "");
+			String returner = Newest.replaceFirst(",", "");
+
+			return (splitNumbers(returner));
+			}
+		}
 
  
 	private static int sum(String[] numbers){
